3646807bd08a6cd736d85a5c8cb1f6b3cee87aba
AndriyOrdynat <andrijordinat391@gmail.com>
2025-04-19 17:42:19 +0300

    changed sheare to shered

--- temp_loger.py
+++ temp_loger.py
@@ -23,18 +23,18 @@ class Saver:
 class Logger:
     def __init__(self, file_name):
         self.filename = file_name
-        self.sheare_data = Saver()
+        self.shared_data = Saver()
         self.log_counter = 0
         with open(self.filename, 'w') as file:
             file.write("Time                Temperature\n")
 
     def log_temperature(self, log_time):
         for i in range(log_time):
-            self.sheare_data.save_now()
-            last_data = self.shared_data.get_data()[-1]
+            self.shared_data.save_now()
+            last_data = self.shared_data.get_data()[i-1]
             with open(self.filename, 'a') as file:
-                file.write(f"{last_data['Time']}                     {last_data[i-1]['Temperature']}\n")
+                file.write(f"{last_data['Time']}                  {last_data['Temperature']}\n")
             time.sleep(1)
 
-log = Logger("My measurements")
+log = Logger("My_measurements.txt")
 log.log_temperature(5)
